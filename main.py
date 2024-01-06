00f27d467f79ab4ed884521e1f5fb13dd8c9cf0f
Naveed <sknaveed513@gmail.com>
2024-01-06 17:58:31 +0530

    evaluation added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
-
+from cnnClassifier.pipeline.stage_04_evaluation import ModelEvaluationPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 
@@ -38,3 +38,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = 'Evaluation stage'
+
+try:
+    logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<')
+    obj = ModelEvaluationPipeline()
+    obj.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x')
+except Exception as e:
+    logger.exception(e)
+    raise e    
